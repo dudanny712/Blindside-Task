a0a77c52e7addda2640a8626f5c3988ec3181f81
dudanny712 <prudhviraj0705@gmail.com>
2022-05-09 12:53:54 +0200

    added different components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 
+import { Container } from 'react-bootstrap';
 import './App.css';
+import Header from './components/header/Header';
+import Sidebar from './components/siderbar/Sidebar';
+import Homescreen from './screens/Homescreen/Homescreen';
 
 function App() {
   return (
     <div className="App">
       
-        
-          Learn React
+        <Header/>
+        <div className="app_container">
+          <Sidebar/>
+          <Container fluid className="app_main">
+              <Homescreen/>
+          </Container>
+        </div>
+         
        
     </div>
   );
---
+++ src/screens/Homescreen/Homescreen.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+import Categories from '../../components/categories/Categories';
+import Video from '../../components/videoscomponent/Video';
+import './_homescreen.scss';
+const Homescreen = () => {
+  return (
+    <div>
+        <Container>
+            <Categories/>
+      <Row>
+        {[...new Array(20)].map(() =>(
+          <Col Lg={3} md={4}>
+            <Video/>
+          </Col>
+        ))}
+        </Row>
+        </Container>
+    </div>
+  )
+}
+
+export default Homescreen
